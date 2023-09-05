154a5ad63fb35dc941e5e3e2738c1737a5ec0386
kerlosbahaa3 <aboelking4@gmail.com>
2023-09-05 16:06:52 +0300

    Add-data

--- schema.sql
+++ schema.sql
@@ -1,108 +1,63 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-/* 1-- Create table animals */
+CREATE TABLE animals(
+ id               INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+ name             VARCHAR(20) ,
+ date_of_birth    DATE ,
+ escape_attempts  INTEGER,
+ neutered         BOOLEAN,
+ weight_kg        DECIMAL,
+ species          VARCHAR(30)
+ );
 
-CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg FLOAT NOT NULL);
-
-----------------------------------------------------------
-/* 2-- Update and delete table */
-
--- Add new column called species of type string
-ALTER TABLE animals ADD COLUMN species VARCHAR(50);
-
-----------------------------------------------------------    
-/* 3-- Query multiple tables */
-
---Create a table named owners with the following columns: id as integer (set it as autoincremented primary key), full_name as string and age as integer.
-CREATE TABLE owners (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    full_name VARCHAR(150) NOT NULL,
-    age INT NOT NULL
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(50) , 
+PRIMARY KEY(name)
 );
 
---Create a table named species with the following columns; id as integer (set it as autoincremented primary key), name as string.
-
-CREATE TABLE species (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY,
+full_name VARCHAR(50),
+age INT , 
+PRIMARY KEY(full_name)
 );
 
--- Modify animals table: 
-
------Remove column species
-ALTER TABLE animals DROP COLUMN species;
-
-----Add column species_id which is a foreign key referencing species table;
-ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
-
-----Add column owner_id which is a foreign key referencing the owners table
-ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
-
-----------------------------------------------------------    
-/* 4-- Add JOIN TABLE for visits */
-
--- Create vets table.
-CREATE TABLE vets (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    date_of_graduation DATE NOT NULL
-);
-
---  Create a join table called specializations to handle the relationship between vets and species.
-
-CREATE TABLE specializations (
-    vet_id INTEGER,
-    species_id INTEGER,
-    FOREIGN KEY (vet_id) REFERENCES vets(id),
-    FOREIGN KEY (species_id) REFERENCES species(id),
-    PRIMARY KEY (vet_id, species_id)
-);
-
--- Add a unique constraint to the "id" column in the "animals" table
 ALTER TABLE animals
-ADD CONSTRAINT animals_id_key UNIQUE (id);
+DROP COLUMN species;
+ 
+ALTER TABLE animals
+ADD COLUMN species_id VARCHAR(50),
+ADD CONSTRAINT species_id
+FOREIGN KEY (species_id) REFERENCES species (name);
+SELECT * FROM animals;
 
+ALTER TABLE animals
+ADD COLUMN owner_id VARCHAR(50),
+ADD CONSTRAINT owner_id
+FOREIGN KEY (owner_id) REFERENCES owners (full_name);
+SELECT * FROM animals;
 
---  Create a join table called specializations to handle the relationship between vets and animals.
+ CREATE TABLE vets(
+ id INT GENERATED ALWAYS AS IDENTITY,
+ name VARCHAR(20),
+ age INT,
+ date_of_graduation DATE,
+ PRIMARY KEY(id)
+ );
 
-CREATE TABLE visits (
-    vet_id INTEGER NOT NULL,
-    animal_id INTEGER NOT NULL,
-    visit_date DATE NOT NULL,
-    CONSTRAINT fk_visits_animals
-        FOREIGN KEY (animal_id) REFERENCES animals (id),
-    CONSTRAINT fk_visits_vets
-        FOREIGN KEY (vet_id) REFERENCES vets (id),
-    PRIMARY KEY (vet_id, animal_id, visit_date)
-);
+ CREATE TABLE specializations (
+ vets_id VARCHAR(50) ,
+ species_id VARCHAR(50),
+ FOREIGN KEY (species_id) REFERENCES species (id),
+ FOREIGN KEY (vets_id) REFERENCES vets (id)
+ );
 
-----------------------------------------------------------    
-/* 5-- Database performance Audit */
-
--- Add an email column to your owners table
-ALTER TABLE owners ADD COLUMN email VARCHAR(120);
-
--- Use EXPLAIN ANALYZE on the previous queries to check what is happening. Take screenshots of them - they will be necessary later.
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
-
-
---First query
-CREATE INDEX index_animal ON visits(animal_id);
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-
---SECOND QUERY
-CREATE INDEX ver_id_index ON visits(vet_id) INCLUDE ( animal_id, visits_date) WHERE vet_id=2;
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
-
---Third query
-CREATE INDEX email_index ON owners(email);
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+ CREATE TABLE visits (
+ animal_id INT,
+ vets_id VARCHAR(50) ,
+ date_of_visit DATE,
+ FOREIGN KEY (animal_id) REFERENCES animals (id),
+ FOREIGN KEY (vets_id) REFERENCES vets (id)
+ );
